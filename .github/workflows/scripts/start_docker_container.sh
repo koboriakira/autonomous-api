9f1663cdd787132712c981c88874b30a89eae50a
a-kobori <89172056+a-kobori@users.noreply.github.com>
2023-05-21 20:05:59 +0900

    CI/CD環境の構築

---
+++ .github/workflows/scripts/start_docker_container.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+cd ~/autonomous-api
+docker rm -f autonomous-api || true
+docker build -t autonomous-api .
+docker run -d --name autonomous-api \
+  -p 5020:8080 \
+  -e BOT_USER_OAUTH_TOKEN=$BOT_USER_OAUTH_TOKEN \
+  -e OPENAI_API_KEY=$OPENAI_API_KEY \
+  autonomous-api
